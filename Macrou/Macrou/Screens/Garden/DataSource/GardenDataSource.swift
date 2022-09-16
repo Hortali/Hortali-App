72dfc65abadcae30fba5f36009abfcf8dba33b1c
thallissousa <thallissousa@outlook.com>
2022-09-16 17:03:38 -0300

    Pull request review da tela de Hortas

--- Macrou/Macrou/Screens/Garden/DataSource/GardenDataSource.swift
+++ Macrou/Macrou/Screens/Garden/DataSource/GardenDataSource.swift
@@ -5,33 +5,12 @@ import UIKit
 
 class GardenDataSource: NSObject, UICollectionViewDataSource {
     
-    /* MARK: - Atributos */
-    
-    private weak var gardenProtocol: GardenProtocol?
-    
-    
-    /* MARK: - Construtor */
-    
-    override init() {
-        super.init()
-        
-    }
-    
-    
-    /* MARK: - Encapsulamento */
-    internal func setProtocol(with gProtocol: GardenProtocol) {
-        self.gardenProtocol = gProtocol
-    }
-    
-    
-    
     /* MARK: - Data Sources */
     
     /// Mostra quantas células vão ser mostradas
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
-    
     
     /// Configura uma célula
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -40,8 +19,6 @@ class GardenDataSource: NSObject, UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GardenCell.identifier, for: indexPath) as? GardenCell else {
             return UICollectionViewCell()
         }
-        
-        //        let row = indexPath.row
         
         return cell
     }
--- Macrou/Macrou/Screens/Garden/Views/Cell/GardenCell.swift
+++ Macrou/Macrou/Screens/Garden/Views/Cell/GardenCell.swift
@@ -7,10 +7,12 @@ class GardenCell: UICollectionViewCell {
     
     /* MARK: - Atributos */
     
+    //Views
+    
     /// Imagem que aparecerá dentro das células da Collection
     private let gardenImage: UIImageView = CustomViews.newImage()
     
-    /// Labels de título com largura ajustada
+    /// Label de título com largura ajustada
     private let titleLabel: UILabel = {
         let lbl = CustomViews.newLabel()
         lbl.adjustsFontSizeToFitWidth = true
@@ -19,7 +21,7 @@ class GardenCell: UICollectionViewCell {
         return lbl
     }()
     
-    /// Labels de endereço com largura ajustada
+    /// Label de endereço com largura ajustada
     private let adressLabel: UILabel = {
         let lbl = CustomViews.newLabel()
         lbl.adjustsFontSizeToFitWidth = true
@@ -28,13 +30,14 @@ class GardenCell: UICollectionViewCell {
         return lbl
     }()
     
+    // Outros
+    
     /// Identificador da célula
     static let identifier = "gardenCell"
     
-    
-    // Views
     /// Constraints dinâmicas que mudarão de acordo com o tamanho da tela
     private var dynamicConstraints: [NSLayoutConstraint] = []
+    
     
     
     /* MARK: - Construtor */
@@ -43,13 +46,12 @@ class GardenCell: UICollectionViewCell {
         super.init(frame: frame)
         
         self.setupViews()
+        
+        // TODO: Apenas para teste - Remover
+        self.INFOS_TESTE()
     }
     
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
-    
-    
-    
-    /* MARK: - Encapsulamento */
     
     
     
@@ -58,7 +60,7 @@ class GardenCell: UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.setText()
+        self.setupStaticText()
         self.setupDynamicConstraints()
         self.setupUI()
         
@@ -71,49 +73,37 @@ class GardenCell: UICollectionViewCell {
     
     /// Adiciona os elementos (Views) na tela
     private func setupViews() {
-        
-        self.contentView.addSubview(gardenImage)
-        self.contentView.addSubview(titleLabel)
-        self.contentView.addSubview(adressLabel)
-        
+        self.contentView.addSubview(self.gardenImage)
+        self.contentView.addSubview(self.titleLabel)
+        self.contentView.addSubview(self.adressLabel)
     }
     
     
     /// Personalização da UI
     private func setupUI() {
         
-        //        self.gardenImage.sizeToFit()
-        
-        let gardenImage = "image.jpg"
-        
-        let image = UIImage(named: gardenImage)
-        
         self.layer.masksToBounds = true
         self.layer.cornerRadius = 20
-        self.gardenImage.image = image
-        
     }
     
+    
     /// Define as propriedades dos textos que aparecerão dentro da célula
-    func setText() {
-        
-        let titleSize: CGFloat = 18 //self.getEquivalent(18)
+    func setupStaticText() {
+        let titleSize: CGFloat = 18    //self.getEquivalent(18)
         let subtitleSize: CGFloat = 12 //self.getEquivalent(12)
         
         self.titleLabel.setupText(with: FontInfo(
-            text: "Horta comunitária da Saúde",
             fontSize: titleSize,
             weight: .medium))
+        
         self.adressLabel.setupText(with: FontInfo(
-            text: "R. Paracatu, 66 - Parque Imperial, SP",
             fontSize: subtitleSize,
             weight: .regular))
-        
     }
+    
     
     /// Define as constraints que dependem do tamanho da tela
     private func setupDynamicConstraints() {
-        
         let lateral: CGFloat = 10
         let between: CGFloat = 5
         
@@ -125,10 +115,12 @@ class GardenCell: UICollectionViewCell {
             self.gardenImage.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
             self.gardenImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
             
+            
             self.adressLabel.leadingAnchor.constraint(equalTo: self.gardenImage.leadingAnchor, constant: lateral),
             self.adressLabel.trailingAnchor.constraint(equalTo: self.gardenImage.trailingAnchor, constant: -lateral),
             self.adressLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -lateral),
             self.adressLabel.heightAnchor.constraint(equalToConstant: 12),
+            
             
             self.titleLabel.leadingAnchor.constraint(equalTo: self.gardenImage.leadingAnchor, constant: lateral),
             self.titleLabel.trailingAnchor.constraint(equalTo: self.adressLabel.trailingAnchor),
@@ -138,6 +130,14 @@ class GardenCell: UICollectionViewCell {
         ]
         
         NSLayoutConstraint.activate(self.dynamicConstraints)
+    }
+    
+    private func INFOS_TESTE() {
+        let gardenImage = "image.jpg"
+        let image = UIImage(named: gardenImage)
+        self.gardenImage.image = image
         
+        self.titleLabel.text = "Horta comunitária da Saúde"
+        self.adressLabel.text = "R. Paracatu, 66 - Parque Imperial, SP"
     }
 }
