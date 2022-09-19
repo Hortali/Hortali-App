f24512db7c7b17988e8c6c9ecbb8020278404555
thallissousa <thallissousa@outlook.com>
2022-09-19 17:31:02 -0300

    Criação da collection de Hortas

--- Macrou/Macrou/Screens/Favorite/Controller/FavoriteViewController.swift
+++ Macrou/Macrou/Screens/Favorite/Controller/FavoriteViewController.swift
@@ -16,6 +16,7 @@ class FavoriteViewController: UIViewController {
     
     
     /* Delegate & Data Sources */
+    private let gardenDataSource = GardenDataSource()
 
 
 		
@@ -53,6 +54,6 @@ class FavoriteViewController: UIViewController {
     
     /// Definindo os delegates, data sources e protocolos
     private func setupDelegates() {
-    
+        self.myView.setDataSource(with: gardenDataSource)
     }
 }
--- Macrou/Macrou/Screens/Favorite/View/FavoriteView.swift
+++ Macrou/Macrou/Screens/Favorite/View/FavoriteView.swift
@@ -16,7 +16,7 @@ class FavoriteView: MainView {
     
     /// Collection das hortas
     private let gardenGroup = CollectionGroup()
-        
+    
     
     // Outros
     
@@ -24,11 +24,19 @@ class FavoriteView: MainView {
     private var dynamicConstraints: [NSLayoutConstraint] = []
 		
 		
-    /// Configurações do layout da collection
-    private let collectionFlow: UICollectionViewFlowLayout = {
+    /// Configurações do layout da collection de Alimentos
+    private let foodCollectionFlow: UICollectionViewFlowLayout = {
         let cvFlow = UICollectionViewFlowLayout()
-        // cvFlow.scrollDirection = .vertical
-		     
+        cvFlow.scrollDirection = .horizontal
+        
+        return cvFlow
+    }()
+    
+    /// Configuração do layout da collectoin de Hortas
+    private let gardenCollectionFlow: UICollectionViewFlowLayout = {
+        let cvFlow = UICollectionViewFlowLayout()
+        cvFlow.scrollDirection = .horizontal
+             
         return cvFlow
     }()
 
@@ -40,6 +48,8 @@ class FavoriteView: MainView {
         super.init()
         
         self.setupViews()
+        self.registerCells()
+        self.setupCollectionFlow()
     }
     
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
@@ -47,7 +57,11 @@ class FavoriteView: MainView {
     
     
     /* MARK: - Encapsulamento */
-    
+    /// Encapsulamento que define o dataSource da Collection
+    /// - Parameter dataSource: Encapsulamento do dataSource costumizado da Collection
+    public func setDataSource(with dataSource: GardenDataSource) {
+        self.gardenGroup.collection.dataSource = dataSource
+    }
     
     
 
@@ -65,6 +79,18 @@ class FavoriteView: MainView {
     
     /* MARK: - Configurações */
 
+    /// Registra as células nas collections/table
+    private func registerCells() {
+        self.gardenGroup.collection.register(GardenCell.self, forCellWithReuseIdentifier: GardenCell.identifier)
+//        self.foodGroup.collection.register(, forCellWithReuseIdentifier: <#T##String#>)
+    }
+    
+    /// Define o layout da collection
+    private func setupCollectionFlow() {
+        self.gardenGroup.collection.collectionViewLayout = self.gardenCollectionFlow
+        self.foodGroup.collection.collectionViewLayout = self.foodCollectionFlow
+    }
+    
     /// Adiciona os elementos (Views) na tela
     private func setupViews() {
         self.contentView.addSubview(self.foodGroup)
@@ -75,6 +101,26 @@ class FavoriteView: MainView {
     /// Personalização da UI
     private func setupUI() {
         self.backgroundColor = UIColor(.favoriteBack)
+        
+        let screenReferenceSize: SizeInfo = SizeInfo(screenSize: CGSize(width: 240,
+                                                                        height: 400),
+                                                     dimension: .height)
+        
+        if self.gardenGroup.collection.bounds.width != 0 {
+            self.gardenCollectionFlow.minimumInteritemSpacing = self.getEquivalent(10)
+            self.gardenCollectionFlow.itemSize = CGSize(
+                width: self.gardenGroup.collection.getEquivalent(240, screenReference: screenReferenceSize),
+                height: self.gardenGroup.collection.bounds.height
+            )
+        }
+        
+        if self.foodGroup.collection.bounds.width != 0 {
+            self.foodCollectionFlow.minimumInteritemSpacing = self.getEquivalent(10)
+            self.foodCollectionFlow.itemSize = CGSize(
+                width: self.foodGroup.collection.getEquivalent(240, screenReference: screenReferenceSize),
+                height: self.foodGroup.collection.bounds.height
+            )
+        }
     }
     
     
@@ -116,6 +162,7 @@ class FavoriteView: MainView {
             self.gardenGroup.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: lateral),
             self.gardenGroup.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
             self.gardenGroup.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -lateral),
+            
         ]
         
         NSLayoutConstraint.activate(self.dynamicConstraints)
