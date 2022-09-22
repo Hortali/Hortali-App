87837bac89ac249821f1a46100758c1e24456d77
thallissousa <thallissousa@outlook.com>
2022-09-22 14:20:05 -0300

    Adicionar texto e botões de "Vitaminas"
    [] Fix: Constraints dos botões de vitaminas;

--- Macrou/Macrou/Screens/InfoFood/View/InfoFoodView.swift
+++ Macrou/Macrou/Screens/InfoFood/View/InfoFoodView.swift
@@ -32,6 +32,9 @@ class InfoFoodView: UIView {
     /// Label seção vitaminas
     private let vitaminsLabel = CustomViews.newLabel()
     
+    /// Label seção de "Como Plantar"
+    private let howTo = CustomViews.newLabel()
+    
     /// Label primeira vitmaina
     private let firstVitaminLabel = CustomViews.newLabel()
     
@@ -41,6 +44,15 @@ class InfoFoodView: UIView {
     /// Label terceira vitmaina
     private let thirdVitaminLabel = CustomViews.newLabel()
     
+    
+    
+    /// Label de informações das vitaminas
+    private let vitaminsInfoLabel = {
+        let lbl = CustomViews.newLabel()
+        lbl.numberOfLines = 2
+        lbl.textColor = UIColor(AppColors.paragraph)
+        return lbl
+    }()
     
     // Outros
     
@@ -132,9 +144,15 @@ class InfoFoodView: UIView {
         
         self.container.contentView.addSubview(self.expansiveLabel)
         self.container.contentView.addSubview(self.vitaminsLabel)
+        self.container.contentView.addSubview(self.howTo)
         self.container.contentView.addSubview(self.firstVitaminLabel)
         self.container.contentView.addSubview(self.secondVitaminLabel)
         self.container.contentView.addSubview(self.thirdVitaminLabel)
+        self.container.contentView.addSubview(self.vitaminsInfoLabel)
+        
+        self.firstVitaminLabel.backgroundColor = .red
+        self.secondVitaminLabel.backgroundColor = .blue
+        self.thirdVitaminLabel.backgroundColor = .green
     }
     
     
@@ -152,7 +170,8 @@ class InfoFoodView: UIView {
         /* Labels */
         self.container.setTitleText(with: "Morango")
         self.vitaminsLabel.setupText(with: FontInfo(text: "Vitaminas", fontSize: 25, weight: .medium))
-        
+        self.howTo.setupText(with: FontInfo(text: "Como plantar", fontSize: 25, weight: .medium))
+
 
         /* Botões */
         let btSize: CGFloat = self.getEquivalent(22)
@@ -173,6 +192,7 @@ class InfoFoodView: UIView {
         let lateral = self.getEquivalent(15)
         let between = self.getEquivalent(20)
         let gap = self.getEquivalent(25)
+        let vitaminsGap = self.getEquivalent(72)
         
         let safeAreaGap = self.scrollView.scroll.safeAreaInsets.top
         
@@ -224,15 +244,39 @@ class InfoFoodView: UIView {
             self.expansiveLabel.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor, constant: -lateral),
             self.expansiveLabel.heightAnchor.constraint(equalToConstant: expLabelHeight),
             
+            
             self.vitaminsLabel.topAnchor.constraint(equalTo: self.expansiveLabel.bottomAnchor, constant: between),
             self.vitaminsLabel.leadingAnchor.constraint(equalTo: self.container.contentView.leadingAnchor),
             self.vitaminsLabel.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor),
             self.vitaminsLabel.heightAnchor.constraint(equalToConstant: gap),
             
-            self.firstVitaminLabel.topAnchor.constraint(equalTo: self.expansiveLabel.bottomAnchor, constant: between),
-            self.firstVitaminLabel.leadingAnchor.constraint(equalTo: self.container.contentView.leadingAnchor),
-            self.firstVitaminLabel.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor),
+            self.firstVitaminLabel.topAnchor.constraint(equalTo: self.vitaminsLabel.bottomAnchor, constant: lateral),
+            self.firstVitaminLabel.leadingAnchor.constraint(equalTo: self.container.contentView.leadingAnchor, constant: 55),
+            self.firstVitaminLabel.widthAnchor.constraint(equalToConstant: gap),
             self.firstVitaminLabel.heightAnchor.constraint(equalToConstant: gap),
+            
+           
+            self.thirdVitaminLabel.topAnchor.constraint(equalTo: self.vitaminsLabel.bottomAnchor, constant: lateral),
+            self.thirdVitaminLabel.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor, constant: -55),
+            self.thirdVitaminLabel.heightAnchor.constraint(equalToConstant: gap),
+            self.thirdVitaminLabel.heightAnchor.constraint(equalToConstant: gap),
+            
+            self.secondVitaminLabel.topAnchor.constraint(equalTo: self.vitaminsLabel.bottomAnchor, constant: lateral),
+            self.secondVitaminLabel.leadingAnchor.constraint(equalTo: self.firstVitaminLabel.trailingAnchor, constant: vitaminsGap),
+            self.secondVitaminLabel.trailingAnchor.constraint(equalTo: self.thirdVitaminLabel.leadingAnchor, constant: vitaminsGap),
+            
+            self.vitaminsInfoLabel.topAnchor.constraint(equalTo: firstVitaminLabel.bottomAnchor, constant: lateral),
+            self.vitaminsInfoLabel.leadingAnchor.constraint(equalTo: self.container.contentView.leadingAnchor),
+            self.vitaminsInfoLabel.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor),
+            self.vitaminsInfoLabel.heightAnchor.constraint(equalToConstant: getEquivalent(65)),
+            
+            self.howTo.topAnchor.constraint(equalTo: vitaminsInfoLabel.bottomAnchor, constant: between),
+            self.howTo.leadingAnchor.constraint(equalTo: self.container.contentView.leadingAnchor),
+            self.howTo.trailingAnchor.constraint(equalTo: self.container.contentView.trailingAnchor),
+            self.howTo.heightAnchor.constraint(equalToConstant: getEquivalent(65))
+            
+            
+           
         ]
         
         NSLayoutConstraint.activate(self.dynamicConstraints)
