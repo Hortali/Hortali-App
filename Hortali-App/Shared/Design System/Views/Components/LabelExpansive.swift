953d0f96f80cd03f96730c1fc00af3364deaab4f
Gui Reis <gui.sreis25@gmail.com>
2022-10-13 00:38:30 -0300

    Atualizar espaço extra na label expandivel

--- Hortali-App/Shared/Design System/Views/Components/LabelExpansive.swift
+++ Hortali-App/Shared/Design System/Views/Components/LabelExpansive.swift
@@ -11,6 +11,16 @@ class ExpansiveLabel: UIView {
 
     // Views
     
+    /// Label de referencia para pegar o tamanho quando tiver texto grande
+    private let invisibleLabel: UILabel = {
+        let lbl = CustomViews.newLabel()
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.sizeToFit()
+        lbl.isHidden = true
+        return lbl
+    }()
+    
     /// Label para colocar o texto expandivel
     private let paragraphLabel: UILabel = {
         let lbl = CustomViews.newLabel()
@@ -23,7 +33,7 @@ class ExpansiveLabel: UIView {
     /// Botão para expandir a label
     private let expansiveButton: CustomButton = {
         let but: CustomButton = CustomViews.newButton()
-        but.backgroundColor = UIColor(originalColor: .greenLight)
+        but.backgroundColor =  UIColor(originalColor: .greenLight)
         but.tintColor = UIColor(originalColor: .greenDark)
         return but
     }()
@@ -55,25 +65,7 @@ class ExpansiveLabel: UIView {
     /// Estado se está expandida
     private var status = false
     
-    
-    /* Encapsulamento -> (Variáveis Computáveis) */
-    
-    /// Tamanho atual da label
-    public var actualLabelSize: CGFloat {
-        return self.lblNormalSize + self.expandedSize
-    }
-    
-    /// Tamanho da expansão da label
-    public var expandedLabelSize: CGFloat {
-        return self.expandedSize
-    }
-    
-    /// Estado se está ou não expandida
-    public var isExtended: Bool {
-        return self.status
-    }
-
-    
+        
     
     /* MARK: - Construtor */
     
@@ -90,11 +82,32 @@ class ExpansiveLabel: UIView {
     
     
     /* MARK: - Encapsulamento */
-
+    
+    /* Variáveis Computáveis */
+    
+    /// Tamanho atual da label
+    public var actualLabelSize: CGFloat {
+        return self.lblNormalSize + self.expandedSize
+    }
+    
+    /// Tamanho da expansão da label
+    public var expandedLabelSize: CGFloat {
+        return self.expandedSize
+    }
+    
+    /// Estado se está ou não expandida
+    public var isExtended: Bool {
+        return self.status
+    }
+    
+    
+    /* Gerais */
+    
     /// Define o texto que a label vai receber
     /// - Parameter text: texto que vai ser definido
     public func setInfoText(for text: String) {
         self.paragraphLabel.text = text
+        self.invisibleLabel.text = text
         
         if text.count < self.textCoundFit {
             self.expansiveButton.isHidden = true
@@ -107,14 +120,10 @@ class ExpansiveLabel: UIView {
     public func setupExtension(extended: Bool) {
         self.status = extended
         
-        var extensionSize: CGFloat = 0
-        
+        var size = self.btNormalSize + self.lblNormalSize
         if self.isExtended {
-            let expandSize: Int = self.textCoundFit * (self.getTextCount() / self.textCoundFit)
-            extensionSize = CGFloat(expandSize)
+            size += round(self.invisibleLabel.bounds.height)
         }
-        
-        let size = self.btNormalSize + self.lblNormalSize + extensionSize
         
         self.setLabelSize(for: size)
     }
@@ -192,6 +201,7 @@ class ExpansiveLabel: UIView {
     
     /// Adiciona os elementos (Views) na tela
     private func setupViews() {
+        self.addSubview(self.invisibleLabel)
         self.addSubview(self.paragraphLabel)
         self.addSubview(self.expansiveButton)
     }
@@ -200,9 +210,10 @@ class ExpansiveLabel: UIView {
     /// Define os textos que são estáticos (os textos em si que vão sempre ser o mesmo)
     private func setupStaticTexts() {		
         /* Labels */
-        self.paragraphLabel.setupText(with: FontInfo(
-            fontSize: 20, weight: .regular
-        ))
+        let fontInfo = FontInfo(fontSize: 20, weight: .regular)
+        
+        self.paragraphLabel.setupText(with: fontInfo)
+        self.invisibleLabel.setupText(with: fontInfo)
         
         /* Botões */
         self.setupButtonIcon()
@@ -220,6 +231,11 @@ class ExpansiveLabel: UIView {
             self.paragraphLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.paragraphLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.paragraphLabel.bottomAnchor.constraint(equalTo: self.expansiveButton.topAnchor),
+            
+            
+            self.invisibleLabel.topAnchor.constraint(equalTo: self.paragraphLabel.topAnchor),
+            self.invisibleLabel.leadingAnchor.constraint(equalTo: self.paragraphLabel.leadingAnchor),
+            self.invisibleLabel.trailingAnchor.constraint(equalTo: self.paragraphLabel.trailingAnchor),
         
             
             self.expansiveButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
