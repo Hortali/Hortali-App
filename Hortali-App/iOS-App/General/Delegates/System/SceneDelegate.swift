f499c6096994176276437dd6c77e5715872c4b36
thallissousa <thallissousa@outlook.com>
2022-11-08 14:20:19 -0300

    Implementar a extensão de GIF

--- Hortali-App/iOS-App/General/Delegates/System/SceneDelegate.swift
+++ Hortali-App/iOS-App/General/Delegates/System/SceneDelegate.swift
@@ -19,8 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        //self.window?.rootViewController = MainController()
-        self.window?.rootViewController = OnboardingViewController()
+        self.window?.rootViewController = MainController()
+//        self.window?.rootViewController = OnboardingViewController()
         self.window?.makeKeyAndVisible()
     }
 }
--- Hortali-App/iOS-App/Screens/InfoFood/View/InfoFoodView.swift
+++ Hortali-App/iOS-App/Screens/InfoFood/View/InfoFoodView.swift
@@ -10,9 +10,6 @@ class InfoFoodView: UIView {
     
     /* MARK: - Atributos */
     
-    // Teste
-    let gifURL: String = "https://c.tenor.com/jhEQIOqHDeEAAAAd/horizontal-nahorizontal.gif"
-    
     // Views
     
     /// Scroll View da tela
@@ -195,12 +192,8 @@ class InfoFoodView: UIView {
     /// Configura a view a partir dos dados recebidos
     /// - Parameter data: dados recebidos
     private func setupViewFor(data: ManagedFood) {
-        
-        self.coverImage.image = UIImage.gifImageWithURL(gifURL)
-        
-        coverImage.frame = CGRect(x: 20, y: 30, width: container.frame.width - 30, height: self.container.frame.height - 30)
+        self.coverImage.image = UIImage(named: data.pageImage.name)
 
-        
         self.container.setTitleText(with: data.name)
         self.expansiveLabel.setInfoText(for: data.benefits)
         
