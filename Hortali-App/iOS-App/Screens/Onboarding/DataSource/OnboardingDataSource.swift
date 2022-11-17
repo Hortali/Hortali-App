a311f12c4df34077758602f66389fe96dd97d81b
Gui Reis <gui.sreis25@gmail.com>
2022-11-17 10:58:15 -0300

    Arrumar erros do git

--- Hortali-App/iOS-App/Screens/Onboarding/DataSource/OnboardingDataSource.swift
+++ Hortali-App/iOS-App/Screens/Onboarding/DataSource/OnboardingDataSource.swift
@@ -4,40 +4,14 @@
 import UIKit
 
 
-<<<<<<<< HEAD:Hortali-App/iOS-App/Screens/Onboarding/DataSource/OnboardingDataSource.swift
-/// Data source da colelction das hortas
+/// Data source da collection do on boarding
 class OnboardingDataSource: NSObject, UICollectionViewDataSource {
-========
-/// Data Source da collection de horário de funcionamento
-class HourInfoDataSource: NSObject, UICollectionViewDataSource {
->>>>>>>> main:Hortali-App/iOS-App/Screens/HourInfo/Data Source/HourInfoDataSource.swift
-    
-    /* MARK: - Atributos */
-    
-    /// Dados usados no data source
-<<<<<<<< HEAD:Hortali-App/iOS-App/Screens/Onboarding/DataSource/OnboardingDataSource.swift
-    
-========
-    private var mainData: [ManagedHourInfo] = []
-        
-    /// Dados que estão sendo usados no data source
-    public var data: [ManagedHourInfo] {
-        set (newData) { self.mainData = newData }
-        get { self.mainData }
-    }
-
->>>>>>>> main:Hortali-App/iOS-App/Screens/HourInfo/Data Source/HourInfoDataSource.swift
-    
     
     /* MARK: - Data Sources */
     
     /// Mostra quantas células vão ser mostradas
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-<<<<<<<< HEAD:Hortali-App/iOS-App/Screens/Onboarding/DataSource/OnboardingDataSource.swift
         return 4
-========
-        return self.data.count
->>>>>>>> main:Hortali-App/iOS-App/Screens/HourInfo/Data Source/HourInfoDataSource.swift
     }
     
     
@@ -46,10 +20,6 @@ class HourInfoDataSource: NSObject, UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCell.identifier, for: indexPath) as? OnboardingCell else {
             return UICollectionViewCell()
         }
-
-        
-        let data = self.data[indexPath.row]
-        cell.setupCell(for: data)
         
         return cell
     }
