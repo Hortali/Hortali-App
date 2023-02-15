771f763756f489718eb88882bdb459c5b567705c
Gui Reis <gui.sreis25@gmail.com>
2023-02-15 12:03:35 -0300

    Atualizar criação da scroll view

--- Hortali-App/Shared/Design System/Components/CustomScroll/CustomScroll.swift
+++ Hortali-App/Shared/Design System/Components/CustomScroll/CustomScroll.swift
@@ -5,7 +5,7 @@ import UIKit
 
 
 /// Cria uma scroll view e já configura ela de acordo com o tamanho que vai ser definido pra ela
-class CustomScroll: UIView {
+class CustomScroll: UIView, ViewCode {
     
     /* MARK: - Atributos */
     
@@ -20,28 +20,23 @@ class CustomScroll: UIView {
     }()
     
     /// Espaço para colocar os elementos na scroll
-    public let contentView: UIView = {
-        let view = CustomViews.newView()
-        return view
-    }()
+    public let contentView = CustomViews.newView()
     
     
-    // Constraints
+    // Protocol:  View Code
     
     /// Constraints dinâmicas que mudam de acordo com o tamanho da tela
-    private var dynamicConstraints: [NSLayoutConstraint] = []
+    internal var dynamicConstraints: [NSLayoutConstraint] = []
     
     
     // Outros
     
     /// Tamanho da scrollView
     private var scrollContentSize = CGSize() {
-        didSet {
-            self.setupScroollSize()
-        }
+        didSet { self.setupScrollSize() }
     }
     
-    /// Último elemento adicionado na scroll
+    /// Último componente adicionado na scroll
     private var lastViewAdded: UIView?
     
     
@@ -50,9 +45,7 @@ class CustomScroll: UIView {
     
     init() {
         super.init(frame: .zero)
-        self.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.setupViews()
+        self.createView()
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -74,43 +67,17 @@ class CustomScroll: UIView {
     
     override public func layoutSubviews() {
         super.layoutSubviews()
-	      
-        self.setupDynamicConstraints()
-        self.setupScroollSize()
+        
+        self.dynamicCall()
+        self.setupScrollSize()
     }
     
     
     
     /* MARK: - Configurações */
     
-    /// Atualiza o tamanho da scroll a partir da posição do último elemento adicionado
-    public func updateScrollSize() {
-        guard
-            let lastViewAdded = self.lastViewAdded,
-            let superview = self.superview
-        else { return }
-         
-        var height = lastViewAdded.frame.origin.y + lastViewAdded.frame.height
-        
-        if height > self.frame.height {
-            let removeSpace = superview.safeAreaInsets.bottom
-            
-            if (height - removeSpace) > superview.bounds.height {
-                height -= removeSpace
-            }
-        }
-        
-        if height != self.frame.height {
-            self.scrollContentSize = CGSize(
-                width: superview.bounds.width,
-                height: height
-            )
-        }
-    }
-    
-    
     /// Configura o tamanho da scroll e a contentView dela
-    private func setupScroollSize() {
+    private func setupScrollSize() {
         self.scroll.frame = self.bounds
         self.contentView.frame.size = self.scrollContentSize
         
@@ -119,20 +86,59 @@ class CustomScroll: UIView {
     }
     
     
-    /// Adiciona os elementos (Views) na tela
-    private func setupViews() {
+    /// Atualiza o tamanho da scroll a partir da posição do último elemento adicionado
+    public func updateScrollSize() {
+        guard !self.lastViewAdded.isNil, !self.superview.isNil else { return }
+        
+        self.removeAdditionalSpace()
+        self.setupNewScrollSize()
+    }
+    
+    
+    /// Remove espaço adicional dependendo da altura
+    private func removeAdditionalSpace() {
+        var height = lastViewAdded!.frame.origin.y + lastViewAdded!.frame.height
+        
+        if height > self.frame.height {
+            let removeSpace = self.superview!.safeAreaInsets.bottom
+            
+            if (height - removeSpace) > self.superview!.bounds.height {
+                height -= removeSpace
+            }
+        }
+    }
+    
+    
+    /// Determina a nova altura da scroll
+    private func setupNewScrollSize() {
+        var height = lastViewAdded!.frame.origin.y + lastViewAdded!.frame.height
+        
+        guard height != self.frame.height else { return }
+        self.scrollContentSize = CGSize(
+            width: self.superview!.bounds.width,
+            height: height
+        )
+    }
+    
+    
+    
+    /* MARK: - Protocol */
+    
+    /* View Code */
+    
+    internal func setupHierarchy() {
         self.addSubview(self.scroll)
         self.scroll.addSubview(self.contentView)
     }
-    	  
     
-    /// Define as constraints que dependem do tamanho da tela
-    private func setupDynamicConstraints() {
-        let safeAreaTop = self.scroll.safeAreaInsets.top
-        
-        NSLayoutConstraint.deactivate(self.dynamicConstraints)
-        
-        self.dynamicConstraints = [
+    
+    internal func setupView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    internal func setupStaticConstraints() {
+        NSLayoutConstraint.activate([
             self.scroll.topAnchor.constraint(equalTo: self.topAnchor),
             self.scroll.leftAnchor.constraint(equalTo: self.leftAnchor),
             self.scroll.rightAnchor.constraint(equalTo: self.rightAnchor),
@@ -140,13 +146,30 @@ class CustomScroll: UIView {
             self.scroll.widthAnchor.constraint(equalTo: self.widthAnchor),
             
             
-            self.contentView.topAnchor.constraint(equalTo: self.scroll.topAnchor, constant: -safeAreaTop),
             self.contentView.leftAnchor.constraint(equalTo: self.scroll.leftAnchor),
             self.contentView.rightAnchor.constraint(equalTo: self.scroll.rightAnchor),
             self.contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             self.contentView.widthAnchor.constraint(equalTo: self.scroll.widthAnchor),
+        ])
+    }
+    
+    
+    internal func setupDynamicConstraints() {
+        let safeAreaTop = self.scroll.safeAreaInsets.top
+        
+        NSLayoutConstraint.deactivate(self.dynamicConstraints)
+        
+        self.dynamicConstraints = [
+            self.contentView.topAnchor.constraint(equalTo: self.scroll.topAnchor, constant: -safeAreaTop),
         ]
         
         NSLayoutConstraint.activate(self.dynamicConstraints)
     }
+    
+    
+    internal func setupStaticTexts() {}
+    
+    internal func setupUI() {}
+    
+    internal func setupFonts() {}
 }
